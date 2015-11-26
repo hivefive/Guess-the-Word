4147312561c3756a781adc7fcad73b685e4ae2ee
Christoffer Bäckström <christofferb.b@gmail.com>
2015-11-26 12:39:40 +0100

    Added more words and second letter hint

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 	// Declare the words for the game and the time that the user has to find the word
-	var words = ['awesome', 'ass', 'ape', 'alone', 'animal', 'best', 'beast', 'breasts', 'banana', 'center', 'cool', 'circle'];
+	var words = ['awesome', 'ass', 'ape', 'alone', 'animal', 'best', 'beast', 'breasts', 'banana', 'center', 'cool', 'circle', 'destroy', 'dust', 'dumb', 'electric', 'element', 'enter', 'fail', 'fall'];
 	var word = '';
 	var gameStarted = false;
 	var timeLeft = 10;
 	var countDown;
 	// Set the input of the text field of the game to blank
 	$('input[name=inputText]').val('');
-	
-	// Function to start the game and to start the countdown timer		
+
+	// Function to start the game and to start the countdown timer
 	$('#startGame').click(function() {
-	
+
 		word = words[Math.floor(words.length * Math.random())];
 		gameStarted = true;
 		$('#gamePrompt').html('');
@@ -18,7 +18,7 @@ $(document).ready(function() {
 		var timeLeft = 10;
 		var hint = word.substring(0,1);
 		$('#gameHint').html('<h1>The first letter is: ' + hint.toUpperCase() + '</h1>');
-		
+
 		$('#inputText').focus();
 		 {
 			countDown = setInterval(function() {
@@ -27,13 +27,19 @@ $(document).ready(function() {
 					$('#gameTime').html('<h1>TIME RAN OUT</h1>');
 					gameStarted = false;
 				}
+				else if(timeLeft < 5) {
+					$('#gameTime').html('<h1>' + timeLeft + ' seconds left</h1>');
+					timeLeft = timeLeft - 1;
+					var hint = word.substring(0,2);
+					$('#gameHint').html('<h1>The first two letters are: ' + hint.toUpperCase() + '</h1>');
+				}
 				else {
 					$('#gameTime').html('<h1>' + timeLeft + ' seconds left</h1>');
 					timeLeft = timeLeft - 1;
 				}
 			}, 1000);
 		}
-		
+
 	});
 	// Triggered when the user presses enter on the keyboard to enter the input in the text field
 	$('#inputForm').keypress(function(e) {
@@ -61,5 +67,5 @@ $(document).ready(function() {
 			}
 		}
 	});
-	
+
 });
