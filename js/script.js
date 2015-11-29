7ab6ced9f39c0d0ff8c5df9dcaa3dcd66a3030ed
Christoffer Bäckström <christofferb.b@gmail.com>
2015-11-29 13:33:53 +0100

    fixed a bug that made the startGame function overlap itself

--- js/script.js
+++ js/script.js
@@ -17,37 +17,44 @@ $(document).ready(function() {
 	// Set the input of the text field of the game to blank
 	$('input[name=inputText]').val('');
 
+	// Game Function
+	function startGame() {
+	 countDown = setInterval(function() {
+		 if(timeLeft < 1) {
+			 clearInterval(countDown);
+			 $('#gameTime').html('<h1>TIME RAN OUT</h1>');
+			 $('#gameHint').html('<h1>The word was: ' + word + '</h1>');
+			 gameStarted = false;
+		 }
+		 else if(timeLeft < 5) {
+			 $('#gameTime').html('<h1>' + timeLeft + ' seconds left</h1>');
+			 timeLeft = timeLeft - 1;
+			 hint = word.substring(0,2);
+			 $('#gameHint').html('<h1>The first two letters are: ' + hint.toUpperCase() + '</h1>');
+		 }
+		 else {
+			 $('#gameTime').html('<h1>' + timeLeft + ' seconds left</h1>');
+			 timeLeft = timeLeft - 1;
+		 }
+	 }, 1000);
+ 	}
+
 	// Function to start the game and to start the countdown timer
 	$('#startGame').click(function() {
-
+		clearInterval(countDown);
+		timeLeft = 0;
 		word = words[Math.floor(words.length * Math.random())];
 		gameStarted = true;
 		$('#gamePrompt').html('');
 		$('#gameTime').html('');
-		var timeLeft = 10;
+		timeLeft = 10;
 		var hint = word.substring(0,1);
 		$('#gameHint').html('<h1>The first letter is: ' + hint.toUpperCase() + '</h1>');
-
+		word = word.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+    	return letter.toUpperCase();
+		});
 		$('#inputText').focus();
-		 {
-			countDown = setInterval(function() {
-				if(timeLeft < 1) {
-					clearInterval();
-					$('#gameTime').html('<h1>TIME RAN OUT</h1>');
-					gameStarted = false;
-				}
-				else if(timeLeft < 5) {
-					$('#gameTime').html('<h1>' + timeLeft + ' seconds left</h1>');
-					timeLeft = timeLeft - 1;
-					var hint = word.substring(0,2);
-					$('#gameHint').html('<h1>The first two letters are: ' + hint.toUpperCase() + '</h1>');
-				}
-				else {
-					$('#gameTime').html('<h1>' + timeLeft + ' seconds left</h1>');
-					timeLeft = timeLeft - 1;
-				}
-			}, 1000);
-		}
+		startGame();
 
 	});
 	// Triggered when the user presses enter on the keyboard to enter the input in the text field
